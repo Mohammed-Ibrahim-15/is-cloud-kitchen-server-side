abd8ba340c421257bceddee9d5d59e033a79c0bf
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-11-08 01:57:30 +0600

    getting data from mongodb

--- index.js
+++ index.js
@@ -15,11 +15,27 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vekfpge.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('isCloudKitchen').collection('items');
+
+        app.get('/items', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const items = await cursor.toArray();
+            res.send(items)
+
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.error(error))
 
 
 
